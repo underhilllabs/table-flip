4579463b12a2347f186aa1673ea1c9e3424d69aa
Bart Lantz <bartlantz@gmail.com>
2015-02-22 16:30:57 -0700

    updates app to use hiccup templating for main page

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
-                 [http-kit "2.1.16"]])
+                 [http-kit "2.1.16"]
+                 [hiccup "1.0.5"]])
--- src/table_flip/core.clj
+++ src/table_flip/core.clj
@@ -1,33 +1,45 @@
 (ns table-flip.core
   (:require [compojure.core :refer :all]
+            [hiccup.core :as hiccup]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
+
+(defn get-flips
+  "Builds a hashmap of the table-flips."
+  []
+  (let [fliphash (hash-map 
+        "flipping"   "(╯°□°)╯︵ ┻━┻"
+        "patience"   "┬─┬ ノ( ゜-゜ノ)"
+        "pudgy"      "(ノ ゜Д゜)ノ ︵ ┻━┻"
+        "battle"     "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)"
+        "me"         "┬─┬ ︵ /(.□. \\)"
+        "aggravated" "(ﾉಥ益ಥ）ﾉ ┻━┻"
+        "putback"    "(ノ^_^)ノ┻━┻ ┬─┬ ノ( ^_^ノ)"
+        "dude"       "(╯°Д°）╯︵ /(.□ . \\)"
+        "emotional"  "(╯'□')╯︵ ┻━┻"
+        "freakout"   "(ﾉಥДಥ)ﾉ︵┻━┻･/"
+        "hercules"   "(/ .□.)\\ ︵╰(゜Д゜)╯︵ /(.□. \\)"
+        "jedi"       "(._.) ~ ︵ ┻━┻"
+        "bear"       "ʕノ•ᴥ•ʔノ ︵ ┻━┻"
+        "magical"    "(/¯◡ ‿ ◡)/¯ ~ ┻━┻"
+        "robot"      "┗[© ♒ ©]┛ ︵ ┻━┻"
+        "russia"     "ノ┬─┬ノ ︵ ( \\o°o)\\ "
+        "happy"      "┻━┻ ︵ ლ(⌒-⌒ლ)"
+        )]
+    fliphash))
 
 (defn flip
   [flip-type]
-  (let [flips 
-        {
-         "flipping"     "(╯°□°)╯︵ ┻━┻"
-         "patience"   "┬─┬ ノ( ゜-゜ノ)"
-         "pudgy"	    "(ノ ゜Д゜)ノ ︵ ┻━┻"
-         "battle"     "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)"
-         "me"         "┬─┬ ︵ /(.□. \\)"
-         "aggravated" "(ﾉಥ益ಥ）ﾉ ┻━┻"
-         "putback"    "(ノ^_^)ノ┻━┻ ┬─┬ ノ( ^_^ノ)"
-         "dude"       "(╯°Д°）╯︵ /(.□ . \\)"
-         "emotional"  "(╯'□')╯︵ ┻━┻"
-         "freakout"   "(ﾉಥДಥ)ﾉ︵┻━┻･/"
-         "hercules"   "(/ .□.)\\ ︵╰(゜Д゜)╯︵ /(.□. \\)"
-         "jedi"       "(._.) ~ ︵ ┻━┻"
-         "bear"       "ʕノ•ᴥ•ʔノ ︵ ┻━┻"
-         "magical"    "(/¯◡ ‿ ◡)/¯ ~ ┻━┻"
-         "robot"      "┗[© ♒ ©]┛ ︵ ┻━┻"
-         "russia"     "ノ┬─┬ノ ︵ ( \\o°o)\\ "
-         "happy"      "┻━┻ ︵ ლ(⌒-⌒ლ)"
-         }]
-    (get flips flip-type)))
+  (get (get-flips) flip-type))
+
+(defn greeting
+  []
+  (concat (hiccup/html 
+           [:h1 "Hello Table-Flipperz"]
+           [:h2 "The following flips are available:"]
+           [:ul (for [x (keys (get-flips))] [:li (str "/" x )])])))
 
 (defroutes table-flip
-  (GET "/" [] "Hello Table-Flipperz!")
+  (GET "/" [] (greeting))
   (GET "/:name" [name] (flip name)))
 
 (defn -main []
